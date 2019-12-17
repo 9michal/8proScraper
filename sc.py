e8653603d1647c49bfd5f78ae3faca6a12f5022c
Michal <michalprelich@gmail.com>
2019-12-17 14:50:57 +0100

    better

--- sc.py
+++ sc.py
@@ -21,30 +21,15 @@ def price(addr, cn):    # cn - class name
             psoup = 'errb'   # beautifulsoup error
     return psoup
 
-xkom = price("https://www.x-kom.pl/p/516874-smartfon-telefon-xiaomi-redmi-note-8-pro-6-128gb-pearl-white.html", 
-            ['div', 'class', 'y67i6l-4 iVWWNC'])  
-
-mihome = price("https://mi-home.pl/telefony-redmi/redmi-note-8-pro-6gb-64gb-pearl-white",
-            ['span' , 'itemprop', 'price'])
-
-komp = price("https://www.komputronik.pl/product/668178/xiaomi-redmi-note-8-pro-6-128gb-bialy.html",
-            ['span' , 'class', 'proper'])
-
-bestcena = price('https://bestcena.pl/smartfony-i-telefony/xiaomi-redmi-note-8-pro-6-64gb-dual-sim-bialy',
-            ['span', 'class', 'price_amount'])
-
 client = MongoClient('mongodb://localhost:27017/') 
 dbase = client['dbase']
 
 if 'collSC' not in dbase.list_collection_names():
     collSC = dbase.create_collection(name='collSC')
-    toAdd = True
 else:
     collSC = dbase['collSC']
 
 date = str(datetime.date.today())
-
-prices = {'data': date, 'xkom': xkom, 'mihome': mihome, 'komp': komp, 'bestcena': bestcena}
 
 if collSC.count_documents({}) == 0:
     toAdd = True
@@ -56,4 +41,19 @@ else:
         toAdd = False
     
 if toAdd:
+    xkom = price("https://www.x-kom.pl/p/516874-smartfon-telefon-xiaomi-redmi-note-8-pro-6-128gb-pearl-white.html", 
+            ['div', 'class', 'y67i6l-4 iVWWNC'])  
+
+    mihome = price("https://mi-home.pl/telefony-redmi/redmi-note-8-pro-6gb-64gb-pearl-white",
+            ['span' , 'itemprop', 'price'])
+
+    komp = price("https://www.komputronik.pl/product/668178/xiaomi-redmi-note-8-pro-6-128gb-bialy.html",
+            ['span' , 'class', 'proper'])
+
+    bestcena = price('https://bestcena.pl/smartfony-i-telefony/xiaomi-redmi-note-8-pro-6-64gb-dual-sim-bialy',
+            ['span', 'class', 'price_amount'])
+    prices = {'data': date, 'xkom': xkom, 'mihome': mihome, 'komp': komp, 'bestcena': bestcena}
     collSC.insert_one(prices)
+    print('OK')
+else:
+    print('NOT OK')
