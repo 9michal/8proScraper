f39b0d5569ea62594e3c45328799951e1ffc0ce4
Michal <michalprelich@gmail.com>
2019-12-19 14:36:34 +0100

    add for loop

--- sc.py
+++ sc.py
@@ -26,7 +26,7 @@ else:
 
 def connect(addr):
     try:
-        req = requests.get(addr)
+        req = requests.get(addr, timeout=12)
     except:
         return False
     if req.status_code == 200:
@@ -36,7 +36,7 @@ def connect(addr):
 
 
 def price(addr, cn):
-    req = requests.get(addr)
+    req = requests.get(addr, timeout=12)
     try:
         soup = BeautifulSoup(req.text, 'html.parser')
         psoup = soup.find(cn[0],{cn[1]:cn[2]})
@@ -47,33 +47,33 @@ def price(addr, cn):
     return psoup
 
 if toAdd:
-    x = 'https://www.x-kom.pl/p/516874-smartfon-telefon-xiaomi-redmi-note-8-pro-6-128gb-pearl-white.html'
-    if connect(x):
-        xkom = price(x, ['div', 'class', 'y67i6l-4 iVWWNC'])
-    else:
-        xkom = 0
+    ad = []
+    x = ['https://www.x-kom.pl/p/516874-smartfon-telefon-xiaomi-redmi-note-8-pro-6-128gb-pearl-white.html', 
+        ['div', 'class', 'y67i6l-4 iVWWNC']]
+    m = ['https://mi-home.pl/telefony-redmi/redmi-note-8-pro-6gb-64gb-pearl-white',
+        ['span' , 'itemprop', 'price']]
+    k = ['https://www.komputronik.pl/product/668178/xiaomi-redmi-note-8-pro-6-128gb-bialy.html',
+        ['span' , 'class', 'proper']]
+    b = ['https://bestcena.pl/smartfony-i-telefony/xiaomi-redmi-note-8-pro-6-64gb-dual-sim-bialy',
+        ['span', 'class', 'price_amount']]
+    ad.append(x)
+    ad.append(m)
+    ad.append(k)
+    ad.append(b)
 
-    m = 'https://mi-home.pl/telefony-redmi/redmi-note-8-pro-6gb-64gb-pearl-white'
-    if connect(m):
-        mihome = price(m,['span' , 'itemprop', 'price'])
-    else:
-        mihome = 0
+    y = [-1] * len(ad)
 
-    k = 'https://www.komputronik.pl/product/668178/xiaomi-redmi-note-8-pro-6-128gb-bialy.html'
-    if connect(k):
-        komp = price(k, ['span' , 'class', 'proper'])
-    else:
-        komp = 0
+    x = 0
+    for i in ad:
+        if connect(i[0]):
+            y[x] = price(i[0], i[1])
+        else:
+            print('ERR: ' + i[0])
+        x += 1
 
-    b = 'https://bestcena.pl/smartfony-i-telefony/xiaomi-redmi-note-8-pro-6-64gb-dual-sim-bialy'
-    if connect(b):
-        bestcena = price(b, ['span', 'class', 'price_amount'])
-    else:
-        bestcena = 0
-
-    prices = {'data': date, 'xkom': xkom, 'mihome': mihome, 'komp': komp, 'bestcena': bestcena}
+    prices = {'data': date, 'xkom': y[0], 'mihome': y[1], 'komp': y[2], 'bestcena': y[3]}
     collSC.insert_one(prices)
+
     print('OK')
 else:
     print('NOT OK')
-
